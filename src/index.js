ea9552ee6164b3a0843d6a2fb0f1c7c15b598791
Garrel Gabriel <garrelmgabriel@gmail.com>
2023-06-14 17:27:22 +0800

    Include vercel script v2

--- src/index.js
+++ src/index.js
@@ -1,19 +1,61 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App/>
-    </BrowserRouter>
-  </React.StrictMode>
-);
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const express = require('express');
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
+const fs = require('fs');
+const qrcode = require('qrcode');
+const moment = require('moment');
+const path = require('path');
+const app = express();
+const bcrypt = require('bcrypt');
+const port = process.env.PORT || 5050;
+const apiUrl = process.env.PUBLIC_URL;
+const mysql = require('mysql');
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  res.header("Access-Control-Allow-Credentials", "true");
+  next();
+});
+
+// const connection = mysql.createConnection({
+//   host: 'localhost',
+//   user: 'root', // this is the default username for XAMPP
+//   password: '', // this is the default password for XAMPP
+//   database: 'db_adamfitness', // replace with the name of your database
+// });
+// const connection = mysql.createConnection({
+//   host: 'localhost',
+//   user: 'u994941609_root', // this is the default username for XAMPP
+//   password: 'Password1', // this is the default password for XAMPP
+//   database: 'u994941609_db_adamfitness', // replace with the name of your database
+// });
+const connection = mysql.createPool({
+  host: 'srv608.hstgr.io',
+  user: 'u994941609_root', // this is the default username for XAMPP
+  password: '8U5oGzb!B', // this is the default password for XAMPP
+  database: 'u994941609_db_adamfitness', // replace with the name of your database
+});
+
+// connection.connect(function(err) {
+//   if (err) {
+//     console.error('Error connecting to MySQL database:', err);
+//     return;
+//   }
+//   console.log('Connected to MySQL database!');
+// });
+
+app.use(express.json());
+
+app.use(cors({
+  origin: ["http://localhost:3000"],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true,
+}));
+
+app.use(cookieParser())
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}))
